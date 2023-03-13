3b903cded7ba071a107abe05c5dccbd795f1e27f
MaGinarY <MaGinarY@DESKTOP-A7QFV3Q>
2023-03-14 01:01:20 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter the lenght:");
+                string legnhtString = Console.ReadLine();
+                int lenghtInt = Convert.ToInt32(legnhtString);
+
+                if (lenghtInt < 0)
+                {
+                    Console.WriteLine("Please write the pisitive number!");
+                    return;
+                }
+
+                Console.WriteLine("Please enter the width:");
+                string widthString = Console.ReadLine();
+                int widthInt = Convert.ToInt32(widthString);
+
+                if (widthInt < 0)
+                {
+                    Console.WriteLine("Please write the pisitive number!");
+                    return;
+                }
+                var area = Rectangle.CalculateArea(widthInt, lenghtInt);
+
+                Console.WriteLine("Area of your Rectangle is:" + area);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please Write the number!");
+            }
+        }
+    }
+}
+
+public static class Rectangle
+{
+    public static int CalculateArea(int lenght, int width)
+    {
+        int area = lenght * width;
+        return area;
+    }
+}
